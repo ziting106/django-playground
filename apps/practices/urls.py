36d1411913e691e65270a52069ad8aea3e672c47
Angelica Liu <ange83106@gmail.com>
2025-11-22 14:09:36 +0800

    多變數添加

--- apps/practices/urls.py
+++ apps/practices/urls.py
@@ -5,4 +5,6 @@ from apps.practices import views
 urlpatterns = [
     path("hello/", views.hello_world, name="hello_world"),
     path("greeting/", views.greeting, name="greeting"),
+    path("search/", views.search, name="search"),
+    path("products/", views.product_list, name="product_list"),
 ]
--- apps/practices/views.py
+++ apps/practices/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def hello_world(request):
     return render(request, "practices/practices.html")
@@ -6,3 +7,13 @@ def hello_world(request):
 def greeting(request):
     name = "Jenny"
     return render(request, "practices/greeting.html", {"name": name})
+
+def search(request):
+    keyword = request.GET.get("q", "")
+    return HttpResponse("Keyword: " + keyword)
+
+def product_list(request):
+    category = request.GET.get("p", "all")
+    sort = request.GET.get("sort", "newest")
+    page = request.GET.get("page", "1")
+    return HttpResponse(f"分類: {category}, 排序: {sort}, 頁數: {page}")
