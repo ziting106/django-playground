6a9a3c8567b2544d0fdf50e294ed8e2c42563e83
Angelica Liu <ange83106@gmail.com>
2025-11-22 12:46:50 +0800

    安裝排版工具

--- apps/practices/views.py
+++ apps/practices/views.py
@@ -1,12 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("""
-    <html>
-    <body>
-    <h1>Hello, World!</h1>
-    <p>This is a test of the Django framework.</p>
-    </body>
-    </html>
-    """)
+    return render(request, "practices/practices.html")
