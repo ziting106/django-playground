041f21024ba7f6326a788018009af55bce192181
Angelica Liu <ange83106@gmail.com>
2025-11-22 11:37:28 +0800

    將view改成HTML

--- apps/practices/views.py
+++ apps/practices/views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("""
+    <html>
+    <body>
+    <h1>Hello, World!</h1>
+    <p>This is a test of the Django framework.</p>
+    </body>
+    </html>
+    """)
