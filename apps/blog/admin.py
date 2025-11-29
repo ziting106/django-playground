55d59b0a547b463786a7dda9adc323bdd1185f39
Angelica Liu <ange83106@gmail.com>
2025-11-29 14:54:54 +0800

    定義Admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.blog.models import Article, Author, Tag
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "is_published", "created_at"]
+    list_filter = ["is_published", "created_at", "author"]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "created_at"]
+
+
+admin.site.register(Tag)
