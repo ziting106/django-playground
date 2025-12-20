9f81a0541c154daab75fcffef32cc7257999aa13
Angelica Liu <ange83106@gmail.com>
2025-12-20 10:17:39 +0800

    添加blog/edit頁面

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -5,9 +5,10 @@ from apps.blog import views
 app_name = "blog"
 
 urlpatterns = [
-    path("articles/", views.article_list, name="article_list"),
-    path("articles/<int:id>/", views.article_detail, name="article_detail"),
-    path("articles/create/", views.article_create, name="article_create"),
     path("tags/", views.tag_list, name="tag_list"),
     path("authors/", views.author_list, name="author_list"),
+    path("articles/", views.article_list, name="article_list"),
+    path("articles/create/", views.article_create, name="article_create"),
+    path("articles/<int:id>/", views.article_detail, name="article_detail"),
+    path("articles/<int:id>/edit/", views.article_edit, name="article_edit"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
+
 from apps.blog.forms import ArticleForm
+
 # Create your views here.
 from apps.blog.models import Article, Author, Tag
 
@@ -23,7 +25,8 @@ def author_list(request):
     authors = Author.objects.all()
     return render(request, "blog/author_list.html", {"authors": authors})
 
-    
+
+# Create Article
 def article_create(request):
     form = ArticleForm(request.POST or None)
     if form.is_valid():
@@ -31,3 +34,14 @@ def article_create(request):
         return redirect("blog:article_detail", id=article.id)
 
     return render(request, "blog/article_create.html", {"form": form})
+
+
+# Edit Article
+def article_edit(request, id):
+    article = get_object_or_404(Article, id=id)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+        article = form.save()
+        return redirect("blog:article_detail", id=article.id)
+
+    return render(request, "blog/article_edit.html", {"form": form, "article": article})
